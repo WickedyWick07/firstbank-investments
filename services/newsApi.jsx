3a43a3a2c2500c9e7556d05c01d400706f46f611
Shaun <nhlamulosmashaba@gmail.com>
2024-11-02 13:00:29 +0200

    Updated news api

--- services/newsApi.jsx
+++ services/newsApi.jsx
@@ -1,18 +1,20 @@
 import axios from "axios"
 
 
-const API_KEY = 'a15eec9c4cad43efb7c4b52176baef5e'
-const BASE_URL = 'https://newsapi.org/v2/everything';
+const API_KEY = 'pub_57997d5009e132b39b9b8d12565f429e6713a'
+const BASE_URL = 'https://newsdata.io/api/1/news';
 
 export const getNewsArticles = async (query) => {
     try {
         const response = await axios.get(BASE_URL, {
             params: {
-                q: query,
-                apiKey: API_KEY
+                category: "business",
+                apiKey: API_KEY,
+                q: "finance,stocks,market",
+                language: 'en'
             }
         });
-        return response.data.articles;
+        return response.data.results;
     } catch (error) {
         console.error('Error fetching news articles:', error);
         throw error;
